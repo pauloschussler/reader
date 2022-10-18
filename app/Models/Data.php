ba4700dc1aee845b022b19fd2e4d2e14a6f02e02
Paulo <paulojuniorpjs@hotmail.com>
2022-10-18 17:19:37 -0300

    feat: Creating Model of Data.

---
+++ app/Models/Data.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Data extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'cpf',
+        'private',
+        'incompleto',
+        'data_ultima_compra',
+        'ticket_medio',
+        'ticket_ultima_compra',
+        'loja_mais_frequente',
+        'loja_ultima_compra',
+    ];
+}
---
+++ resources/js/componets/data/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Index = () => {
+
+    return (
+
+        <div>
+            <h1>Index data page</h1>
+        </div>
+    )
+
+}
