592a83c954e7ba4b89c1cdf729a4e6b9d5c1df24
Paulo <paulojuniorpjs@hotmail.com>
2022-10-17 22:34:20 -0300

    feat: Added React to project.

---
+++ resources/js/app.jsx
@@ -0,0 +1,10 @@
+import './bootstrap';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './componets/App';
+
+ReactDOM.createRoot(document.getElementById('app')).render(
+    <App />
+)
