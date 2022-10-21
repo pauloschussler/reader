09fe5d5c4ad336070c2e66d7f5743560acddd8b6
Paulo <paulojuniorpjs@hotmail.com>
2022-10-21 14:15:38 -0300

    feat: Dockerization of project, ngix and php server.

--- routes/web.php
+++ routes/web.php
@@ -16,22 +16,27 @@ use App\Http\Controllers\RecordsController;
 |
 */
 
-Route::resource('records', RecordsController::class);
+// Route::resource('records', RecordsController::class);
+
+// Route::get('/', function () {
+//     // return Inertia::render('Dashboard', [
+//     //     'canLogin' => Route::has('login'),
+//     //     'canRegister' => Route::has('register'),
+//     //     'laravelVersion' => Application::VERSION,
+//     //     'phpVersion' => PHP_VERSION,
+//     // ]);
+
+// });
+
+// Route::get('/dashboard', function () {
+//     return Inertia::render('Dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+// require __DIR__ . '/auth.php';
+
+// Route::get('file-upload', [FileController::class, 'index'])->name('file.upload');
+// Route::post('file-upload', [FileController::class, 'store'])->name('file.upload.store');
 
 Route::get('/', function () {
-    return Inertia::render('Dashboard', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return view('welcome');
 });
-
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-require __DIR__ . '/auth.php';
-
-Route::get('file-upload', [FileController::class, 'index'])->name('file.upload');
-Route::post('file-upload', [FileController::class, 'store'])->name('file.upload.store');
