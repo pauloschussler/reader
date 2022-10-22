047b2cc6c0ff26513fb09ad2ca04e89cc9e0732a
Paulo Junior Schüssler <paulojuniorpjs@hotmail.com>
2022-10-21 23:18:28 -0300

    feat: File upload.

--- app/Http/Controllers/FilesController.php
+++ app/Http/Controllers/FilesController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Validator;
 use App\Models\Files;
+use App\Models\Records;
 use Illuminate\Support\Facades\Auth;
 
 class FilesController extends Controller
@@ -30,6 +31,18 @@ class FilesController extends Controller
         }
     }
 
+    public function create()
+    {
+
+        if (Auth::check()) {
+
+            return Inertia::render('Files/Create');
+        } else {
+
+            return redirect('/dashboard');
+        }
+    }
+
     /**
      * Show the form for creating a new resource.
      *
@@ -40,13 +53,66 @@ class FilesController extends Controller
     {
 
         Validator::make($request->all(), [
-            'title' => 'required',
-            'file' => 'required|mimes:text,csv|max:10240',
+            'file' => 'required|max:10240',
         ])->validate();
 
         $fileName = time() . '.' . $request->file->extension();
-        $request->file->move(public_path('uploads'), $fileName);
 
-        return response()->json(["status" => print_r($request->file)]);
+        $fileLines = file($request->file, FILE_IGNORE_NEW_LINES);
+
+        Files::create([
+            'name' => $fileName,
+            'lines' => sizeof($fileLines),
+            'size' => $request->file->getSize()
+        ]);
+
+        unset($fileLines[0]);
+
+        $data = [];
+
+        foreach ($fileLines as $line) {
+
+            $lineData = preg_replace('/ {2,}/', ',', $line);
+            $lineData = explode(',', $lineData);
+
+            $this->createRecord($lineData);
+        }
+
+
+        // $retorno = explode(",", $output);
+
+
+        return response()->json(['content' =>  '']);
+
+
+        // $newRecords = Records::create([
+        //     'cpf' => $request->cpf,
+        //     'private' => $request->private,
+        //     'incompleto' => $request->incompleto,
+        //     'ticket_medio' => $request->ticket_medio,
+        //     'ticket_ultima_compra' => $request->ticket_ultima_compra,
+        //     'loja_mais_frequente' => $request->loja_mais_frequente,
+        //     'loja_ultima_compra' => $request->loja_ultima_compra
+        // ]);
+
+        // if ($newRecords) {
+
+        //     return response()->json(["status" => 200]);
+        // }
+    }
+
+    private function createRecord(array $record)
+    {
+
+        Records::create([
+            'cpf' => isset($record[0]) && $record[0] != "NULL" ? $record[0] : null,
+            'private' => isset($record[1]) && $record[1] != "NULL" ? $record[1] : null,
+            'incompleto' => isset($record[2]) && $record[2] != "NULL" ? $record[2] : null,
+            'data_ultima_compra' => isset($record[3]) && $record[3] != "NULL" ? $record[3] : null,
+            'ticket_medio' => isset($record[4]) && $record[4] != "NULL" ? $record[4] : null,
+            'ticket_ultima_compra' => isset($record[5]) && $record[5] != "NULL" ? $record[5] : null,
+            'loja_mais_frequente' => isset($record[6]) && $record[6] != "NULL" ? $record[6] : null,
+            'loja_ultima_compra' => isset($record[7]) && $record[7] != "NULL" ? $record[7] : null,
+        ]);
     }
 }
